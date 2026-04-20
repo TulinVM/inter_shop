29e2925e24f98939ebd29ff8d2e4df4769d62f2f
Viktor <TulinVM@gmail.com>
2026-04-20 16:59:40 +0300

    Edit

--- products/models.py
+++ products/models.py
@@ -17,7 +17,7 @@ class ProductCategory(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images', null=True, blank=True)
